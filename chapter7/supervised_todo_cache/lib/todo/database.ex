800e93aadd383c27cccbceaf3b8d6a1042cbd187
Dominique VASSARD <dominique.vassard@gmail.com>
2022-04-01 20:36:31 +0200

    chapter7: Supervisor

---
+++ chapter7/supervised_todo_cache/lib/todo/database.ex
@@ -0,0 +1,79 @@
+defmodule Todo.Database do
+  alias Todo.RoundRobin
+  use GenServer
+
+  @db_folder "./persist"
+
+  def start_link do
+    IO.puts("Starting database server")
+    GenServer.start_link(__MODULE__, nil, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_) do
+    File.mkdir_p!(@db_folder)
+
+    worker_list =
+      Enum.map(1..3, fn index ->
+        worker_name = :"worker#{index}"
+        Todo.DatabaseWorker.start_link(@db_folder, worker_name)
+        worker_name
+      end)
+
+    {:ok, {RoundRobin.from_list(worker_list), %{}}}
+  end
+
+  def store(key, data) do
+    GenServer.cast(__MODULE__, {:store, key, data})
+  end
+
+  def get(key) do
+    GenServer.call(__MODULE__, {:get, key})
+  end
+
+  def delete(key) do
+    GenServer.cast(__MODULE__, {:delete, key})
+  end
+
+  @impl true
+  def handle_cast({:store, key, data}, state) do
+    {worker, worker_list, registry} =
+      key
+      |> worker(state)
+
+    Todo.DatabaseWorker.store(worker, key, data)
+
+    {:noreply, {worker_list, registry}}
+  end
+
+  @impl true
+  def handle_cast({:delete, key}, state) do
+    {worker, worker_list, registry} =
+      key
+      |> worker(state)
+
+    Todo.DatabaseWorker.delete(worker, key)
+
+    {:noreply, {worker_list, registry}}
+  end
+
+  @impl true
+  def handle_call({:get, key}, _, state) do
+    {worker, worker_list, registry} =
+      key
+      |> worker(state)
+
+    {:reply, Todo.DatabaseWorker.get(worker, key), {worker_list, registry}}
+  end
+
+  defp worker(key, {worker_list, registry}) do
+    case Map.fetch(registry, key) do
+      {:ok, worker} ->
+        {worker, worker_list, registry}
+
+      :error ->
+        {RoundRobin.current(worker_list), RoundRobin.next(worker_list),
+         Map.put(registry, key, RoundRobin.current(worker_list))}
+    end
+  end
+end
---
+++ chapter7/supervised_todo_cache/lib/todo/database_worker.ex
@@ -0,0 +1,71 @@
+defmodule Todo.DatabaseWorker do
+  use GenServer
+
+  def start_link(db_folder, name) do
+    IO.puts("Starting database worker")
+    GenServer.start_link(__MODULE__, db_folder, name: name)
+  end
+
+  @impl true
+  def init(db_folder) do
+    {:ok, db_folder}
+  end
+
+  def store(worker_name, key, data) do
+    GenServer.cast(worker_name, {:store, key, data})
+  end
+
+  def get(worker_name, key) do
+    GenServer.call(worker_name, {:get, key})
+  end
+
+  def delete(worker_name, key) do
+    GenServer.cast(worker_name, {:delete, key})
+  end
+
+  @impl true
+  def handle_cast({:store, key, data}, db_folder) do
+    spawn(fn ->
+      key
+      |> filename(db_folder)
+      |> File.write!(:erlang.term_to_binary(data))
+    end)
+
+    {:noreply, db_folder}
+  end
+
+  @impl true
+  def handle_cast({:delete, key}, db_folder) do
+    spawn(fn ->
+      key
+      |> filename(db_folder)
+      |> File.rm!()
+    end)
+
+    {:noreply, db_folder}
+  end
+
+  @impl true
+  def handle_call({:get, key}, caller, db_folder) do
+    spawn(fn ->
+      fetched_data =
+        key
+        |> filename(db_folder)
+        |> File.read()
+
+      data =
+        case fetched_data do
+          {:ok, new_data} -> :erlang.binary_to_term(new_data)
+          {:error, _} -> nil
+        end
+
+      GenServer.reply(caller, data)
+    end)
+
+    {:noreply, db_folder}
+  end
+
+  defp filename(key, db_folder) do
+    Path.join(db_folder, to_string(key))
+  end
+end
